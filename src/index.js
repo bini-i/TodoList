1c0d13f363c053fd53951d690bbb52df554cc09d
MrSayyod <ssayyod@gmail.com>
2021-03-13 00:01:48 +0500

    Create default array for todos when project is not selected

--- src/index.js
+++ src/index.js
@@ -13,3 +13,9 @@ let example = new Todo("job", "solve", "13-03-21", "medium")
 console.log(example.title);
 console.log(example);
 
+// create a project array. array should contains of todo objects. it will be a default project. when user creates a todo, the todo will automatically will be inside of the default project. user can create a new project and todos inside of it.
+
+const defaultProject = []
+if (!projectName) {  //there is no project name/ user not created any project
+  defaultProject.push(new Todo) //created todos by user should be pushed to defaultProject
+}
