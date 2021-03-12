8824dfa9adbfd47332bef7aa0f5161fb02788112
bini-i <binyam.sh1@gmail.com>
2021-03-12 23:53:06 +0300

    Add home.js
    - renders form html and list of todolist using js

---
+++ src/home.js
@@ -0,0 +1,41 @@
+import {domNodeCreator, chainAppend } from './domNodeCreator'
+
+export default () => {
+    //render home page
+    console.log('here')
+    const content = document.getElementById('content')
+    const todoList = domNodeCreator('div', {class: 'todo-list body-padding'})
+    const h1 = domNodeCreator('h1', {}, 'Todo List')
+    const form = domNodeCreator('form', {class: 'mb-3'})
+    let arr = ['Title', 'Description', 'Due date']
+    arr.forEach((ele)=>{
+        const group = domNodeCreator('div', {class: 'form-group col-md-7'})
+        const input = domNodeCreator('input', {type: 'text', class: 'form-control', placeholder: ele})
+        chainAppend([form, group, input])
+    })
+
+    const selectGroup = domNodeCreator('div', {class: 'form-group col-md-4'})
+    const select = domNodeCreator('select', {class: 'form-control'}) 
+    arr = ['Priority', 'Low', 'Medium', 'High']
+    arr.forEach((ele)=>{
+        const option = domNodeCreator('option', {}, ele)
+        select.appendChild(option)
+    })
+    chainAppend([form, selectGroup, select])
+
+    const button = domNodeCreator('button', {type: 'submit', class: 'btn btn-primary'}, 'Add')
+    form.appendChild(button)
+
+    const ul = domNodeCreator('ul', {class: 'list-group list-group-flush'})
+    const li = domNodeCreator('li', {class: 'list-group-item'})
+    const title = domNodeCreator('span', {}, 'lorem ipsum')
+    const remove = domNodeCreator('span')
+    const removeIcon = domNodeCreator('i', {class: 'fa fa-times'})
+
+    chainAppend([li, title])
+    chainAppend([li, remove, removeIcon])
+
+    chainAppend([content, todoList, h1])
+    chainAppend([content, todoList, form])
+    chainAppend([content, todoList, ul, li])
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import renderHome from './home.js'
 class Todo {
   constructor(title, description, dueDate, priority) {
     this.title = title
@@ -8,14 +8,16 @@ class Todo {
   }
 }
 
-
 let example = new Todo("job", "solve", "13-03-21", "medium")
 console.log(example.title);
 console.log(example);
 
 // create a project array. array should contains of todo objects. it will be a default project. when user creates a todo, the todo will automatically will be inside of the default project. user can create a new project and todos inside of it.
 
-const defaultProject = []
-if (!projectName) {  //there is no project name/ user not created any project
-  defaultProject.push(new Todo) //created todos by user should be pushed to defaultProject
-}
+// const defaultProject = []
+// if (!projectName) {  //there is no project name/ user not created any project
+//   defaultProject.push(new Todo) //created todos by user should be pushed to defaultProject
+// }
+
+console.log('index.js')
+renderHome()
