455bed6d4da59d8e9bc9a396a65c9a7de537189c
MrSayyod <ssayyod@gmail.com>
2021-05-07 03:51:10 -0700

    Separate modules from factory.js for testing and modify projectModule.js

---
+++ src/projectModule.js
@@ -0,0 +1,22 @@
+export default (() => {
+  let project = { 0: 'default' };
+  function projectFactory(name) {
+    const lastKey = Object.keys(project)[Object.keys(project).length - 1];
+    project[parseInt(lastKey, 10) + 1] = name;
+    localStorage.setItem('project', JSON.stringify(project))
+    return name
+  }
+  return {
+    createProject: projectFactory,
+    getProject: (projectId) => project[projectId],
+    getAllProject: () => Object.assign(project),
+    loadProject: ()=> {
+      const savedProject = JSON.parse(localStorage.getItem('project'))
+      if (savedProject) {
+        project = {...project, ...storedProject}
+      }
+      return savedProject
+    }
+  };
+})();
+
